057b14356231c526b7858203e93272122cfb4b6c
fazt <fazt@faztweb.com>
2023-03-13 18:45:44 -0500

    added README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Django React CRUD
+
+This is a simple Django React CRUD application.
+
+# Environment variables
+
+- VITE_BACKEND_URL, this is neccessary in production, it should be the url of the backend server
--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const URL =
-  process.env.NODE_ENV === "production" ? import.meta.env.VITE_BACKEND_URL : "http://localhost:8000";
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_BACKEND_URL
+    : "http://localhost:8000";
 
-console.log(URL)
+console.log(URL);
 const tasksApi = axios.create({
   baseURL: `${URL}/tasks/api/v1/tasks`,
 });
